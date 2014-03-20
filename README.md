6d2abad54e5295ff2dc4ea83b4d9a419b5eb45c7
Erik Dunning <edunning@morgridge.org>
2014-03-20 10:59:14 -0500

    Added the files.

---
+++ README.md
@@ -0,0 +1,3 @@
+=Archer Fortune Quotes and Scraper Script
+
+This is just for some fun command line prompts.  All credit goes to the brilliant writing of Adam Reed and the patronage of FX.
---
+++ quote-grabber.php
@@ -0,0 +1,43 @@
+<?php
+
+
+`wget -O /tmp/page "http://www.tvfanatic.com/quotes/shows/archer/"`;
+
+libxml_use_internal_errors(true);
+
+$page = DOMDocument::loadHTMLFile('/tmp/page');
+$paragraphs = $page->getElementsByTagName('p');
+
+foreach( $paragraphs as $p ){
+	if( stripos( $p->getNodePath(), 'blockquote') ){
+		foreach ($p->childNodes as $pChild) {
+			if ($pChild->nodeType === XML_TEXT_NODE) {
+				file_put_contents('archer', trim($pChild->textContent)."\n", FILE_APPEND);;
+			}
+		}
+		file_put_contents('archer',"%\n", FILE_APPEND);
+	}
+}
+
+for( $i = 2; $i < 56; $i++ ){
+
+	sleep(1);
+
+	`wget -O /tmp/page "http://www.tvfanatic.com/quotes/shows/archer/page-$i.html"`;
+
+	libxml_use_internal_errors(true);
+
+	$page = DOMDocument::loadHTMLFile('/tmp/page');
+	$paragraphs = $page->getElementsByTagName('p');
+
+	foreach( $paragraphs as $p ){
+		if( stripos( $p->getNodePath(), 'blockquote') ){
+			foreach ($p->childNodes as $pChild) {
+				if ($pChild->nodeType === XML_TEXT_NODE) {
+					file_put_contents('archer', trim($pChild->textContent)."\n", FILE_APPEND);;
+				}
+			}
+			file_put_contents('archer',"%\n", FILE_APPEND);
+		}
+	}
+}
